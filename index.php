627db71d017fcc8f5d7d386658b8dff2c79cf918
091910958Ambrosio <ambrosiohernandez12345@gmail.com>
2022-03-14 14:24:43 -0600

    SNAPSHOT 002 del Proyecto solo modificamos index

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
- echo'<h2>Saludos Cordiales</h2>';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<font size="14" tempus="Tempus Sans ITC" color="blue">
+
+    <marquee>
+    SALUDOS CORDIALES TICsitos
+    </marquee>
+    </font>
+</body>
+</html>
